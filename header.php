c290f82fb850f153e2b80d03a47478fb386f0590
ginotrombetti <gdtrombetti@gmail.com>
2015-10-22 14:34:49 -0400

    Added Checkboxes,  removed Nav

--- header.php
+++ header.php
@@ -7,25 +7,3 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 <body>
-	<br/>
-	<div class="navbar navbar-default navbar-fixed-top" id="navbar-main">
-		<div class="container">
-			<div class="navbar-header">
-				<span class="navbar-brand">Contracting Books</span>
-			</div>
-			<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li><a class="item" href="index.php">Home</a></li>
-					<li><a class="item" href="#">Buy Now!</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a class="item" href="https://www.facebook.com">
-					<img src="css/facebook.png"></a></li>
-					<li><a class="item" href="https://www.twitter.com">
-					<img src="css/twitter.png"></a></li>
-					<li><a class="item" href="https://www.linkedin.com">
-					<img src="css/linkedin.png"></a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
--- purchase-info-landing.php
+++ purchase-info-landing.php
@@ -5,18 +5,26 @@ $lf_query = new LF_queries( $conn );
 
 $inputs = filter_input_array( INPUT_POST, FILTER_SANITIZE_STRING );
 echo "<br/><br/>";
+extract($inputs);
 var_dump($inputs);
-		$subject = "Contractor's Book Purchase!";
-		$message = "Hello " . $fname . " we have recieved your purchase!  We need a couple days for processing,  we will send you an email once we send out your book!";
-		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$headers .= "From: Contractor Books";
-		if (@mail( "gdtrombetti@gmail.com", $subject, $message, $headers )) {
+		$subject_h = "An order for the contractor book!";
+		$message_h = "Hello processesor we have recieved an order: 
+						Name: " . $fName .;
+		$headers_h .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers_h .= "From: Contractor Books";
+
+
+		$subject_c = "Contractor's Book Order!";
+		$message_c = "Hello " . $fName . " we have recieved your order!  We need a couple days for processing,  we will send you an email once we send out your book!";
+		$headers_c .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers_c .= "From: Contractor Books";
+		if (@mail( "gdtrombetti@gmail.com", $subject_c, $message_c, $headers_c)) {
 			?>
 			<div class="container">
 				<div class="page-header" id="banner">
 					<div class="row">
 						<div class="jumbotron">
-							Thank You For Your Purchase!  You will be receiving a confirmation email momentarily.
+							Thank You For Your Order!  You will be receiving a confirmation email momentarily.
 						</div>
 					</div>
 				</div>
