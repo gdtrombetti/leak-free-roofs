38369a4708d3289e07ab86a5c348d18c537440eb
ginotrombetti <gdtrombetti@gmail.com>
2015-10-22 19:00:09 -0400

    Adding thank you page

---
+++ thankyoupage.php
@@ -0,0 +1,14 @@
+<?php
+	include 'header.php';
+?>
+<div class="container">
+	<div class="page-header" id="banner">
+		<div class="row">
+			<div class="jumbotron">
+				Thank You For Your Order!  You will be receiving a confirmation email momentarily.
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include 'footer.php';
