ec6050ca4aaa4946e1dddd6f3d06092e5b020c63
ginotrombetti <gdtrombetti@gmail.com>
2015-10-23 19:11:04 -0400

    Added button to front page - needs positioning

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 include 'header.php';
 	?>
-	<div class="container">
-		<iframe src="css/front_page.pdf" title="front" align="top" height="620" width="100%" frameborder="0" scrolling="auto" target="Message">
-		</iframe>
-	</div>
-<?php 
+		<div class="container">
+			<div id="iframe-div">
+				<iframe src="css/front_page.pdf" title="front" align="top" height="792" width="100%" frameborder="0" scrolling="no" target="Message"></iframe>
+				<div class="content"><button type="submit" class="btn btn-primary">Send It Now!</button></div>
+			</div>
+		</div>
+<?php
 include 'footer.php';
